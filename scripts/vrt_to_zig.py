40517ff2c2fa5eef7432158033ad3572b72658e6
Timm Weber <timm.weber@me.com>
2023-01-25 15:33:25 +0100

    refactoring of container header code

--- scripts/vrt_to_zig.py
+++ scripts/vrt_to_zig.py
@@ -42,6 +42,7 @@ base_uuid = UUID('b764b867-cac4-4329-beda-9c021c5184d7') # uuid of base layer co
 tok_uuid = UUID('b7887880-e234-4dd0-8d6a-b8b99397b030') # uuid of first P-attr (token stream)
 pos_uuid = UUID('634575cf-43c2-4a7e-b239-4e0ce2ecb394') # uuid of second P-attr (pos tags)
 
+### Scan VRT file for number of tokens
 clen = 0 # length of corpus, first to be determined by scanning the VRT file
 
 with args.input.open() as f:
@@ -51,38 +52,14 @@ with args.input.open() as f:
                 clen += 1
 print(f'Found input file with {clen} corpus positions')
 
-### Write Base Layer container
+BOM_START = 160
+LEN_BOM_ENTRY = 48
 
-f = (args.output / (str(base_uuid) + '.zigl')).open(mode="wb")
-
-## write header
-# consts
-f.write('Ziggurat'.encode('ascii')) # magic
-f.write('1.0\t'.encode('ascii')) # version
-f.write('Z'.encode('ascii')) # container family
-f.write('L'.encode('ascii')) # container class
-f.write('p'.encode('ascii')) # container type (primary layer)
-f.write('\n'.encode('ascii')) # LF
-
-f.write(str(base_uuid).encode('ascii')) # uuid as ASCII (36 bytes)
-f.write('\n\x04\0\0'.encode('ascii')) # LF EOT 0 0
-
-# components meta
-f.write(pack('B', 1)) #allocated
-f.write(pack('B', 1)) #used
-
-f.write(bytes(6)) # padding
-
-# dimensions
-f.write(pack('<q', clen)) # dim1
-f.write(pack('<q', 0)) # dim2
-
-# referenced base layers
-f.write(bytes(40)) # base1_uuid + padding
-f.write(bytes(40)) # base2_uuid + padding
+def data_start(clen):
+    return BOM_START + (clen * LEN_BOM_ENTRY)
 
 def bom_entry(type, name, mode, offset,
-              size, param1, param2):
+            size, param1, param2):
     assert len(name.encode('ascii')) <= 12
     return pack('B', 1) +\
         pack('B', type) +\
@@ -93,24 +70,71 @@ def bom_entry(type, name, mode, offset,
         pack('<q', param1) +\
         pack('<q', param2)
 
-BOM_START = 160
-LEN_BOM_ENTRY = 48
+def write_container_header(f, ctype, uuid, dimensions, 
+                           base1_uuid, base2_uuid, *bom_entries):
+    # consts
+    f.write('Ziggurat'.encode('ascii')) # magic
+    f.write('1.0\t'.encode('ascii')) # version
+    f.write(ctype[0].encode('ascii')) # container family
+    f.write(ctype[1].encode('ascii')) # container class
+    f.write(ctype[2].encode('ascii')) # container type
+    f.write('\n'.encode('ascii')) # LF
 
-def data_start(clen):
-    return BOM_START + (clen * LEN_BOM_ENTRY)
+    f.write(str(uuid).encode('ascii')) # uuid as ASCII (36 bytes)
+    f.write('\n\x04\0\0'.encode('ascii')) # LF EOT 0 0
 
-## write BOM entries
+    # components meta
+    f.write(pack('B', len(bom_entries))) #allocated
+    f.write(pack('B', len(bom_entries))) #used
 
-# Partition vector
-f.write(bom_entry(
-    0x4,
-    'Partition',
-    0x0,
-    data_start(1), # offset
-    16, # size
-    0xABCD,
-    1
-))
+    f.write(bytes(6)) # padding
+
+    # dimensions
+    f.write(pack('<q', dimensions[0])) # dim1
+    f.write(pack('<q', dimensions[1])) # dim2
+
+    # referenced base layers
+    if base1_uuid:
+        s = str(base1_uuid).encode('ascii')
+        assert len(s) == 36, "UUID must be 36 bytes long"
+        f.write(s)
+    else:
+        f.write(bytes(36)) # base1_uuid + padding
+    f.write(bytes(4)) # padding
+    
+    if base2_uuid:
+        s = str(base2_uuid).encode('ascii')
+        assert len(s) == 36, "UUID must be 36 bytes long"
+        f.write(s)
+    else:
+        f.write(bytes(36)) # base2_uuid + padding
+    f.write(bytes(4)) # padding
+
+    # write BOM entries
+    for entry in bom_entries:
+        f.write(entry)
+
+### Write Base Layer container
+
+f = (args.output / (str(base_uuid) + '.zigl')).open(mode="wb")
+
+## write header
+write_container_header(f,
+    'ZLp',
+    base_uuid,
+    (clen, 0),
+    None,
+    None,
+    bom_entry(
+        0x4,
+        'Partition',
+        0x0,
+        data_start(1), # offset
+        16, # size
+        clen,
+        1
+    )
+)
 
 ## write components
 
