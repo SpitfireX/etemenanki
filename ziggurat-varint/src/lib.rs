75c254746d9e67a4e1c3d72787665e7b22dd53b4
SpitfireX <timm.weber@me.com>
2023-07-05 23:47:38 +0200

    general restructuring and added varint decoding

--- ziggurat-varint/src/lib.rs
+++ ziggurat-varint/src/lib.rs
@@ -1,22 +1,17 @@
-use std::convert::From;
-
 use pyo3::prelude::*;
 use pyo3::types::PyBytes;
 
-/// Formats the sum of two numbers as string.
 #[pyfunction]
 fn encode_varint(py: Python, x: i64) -> PyObject {
     let mut buffer = [0u8; 9];
-    let v: VarInt = x.into();
-    let len = v.encode(&mut buffer);
+    let len = x.encode_varint(&mut buffer);
     PyBytes::new(py, &buffer[..len]).into()
 }
 
 #[pyfunction]
 fn encode_varint_unsigned(py: Python, x: u64) -> PyObject {
     let mut buffer = [0u8; 9];
-    let v: VarInt = (x as i64).into();
-    let len = v.encode(&mut buffer);
+    let len = (x as i64).encode_varint(&mut buffer);
     PyBytes::new(py, &buffer[..len]).into()
 }
 
@@ -25,9 +20,8 @@ fn encode_varint_block(py: Python, ints: Vec<i64>) -> PyObject {
     let mut buffer = vec![0u8; ints.len() * 9];
     let mut blen = 0;
 
-    for int in ints.iter() {
-        let v: VarInt = (*int).into();
-        blen += v.encode(&mut buffer[blen..(blen+9)]);
+    for int in ints {
+        blen += int.encode_varint(&mut buffer[blen..(blen + 9)]);
     }
 
     PyBytes::new(py, &buffer[..blen]).into()
@@ -38,9 +32,8 @@ fn encode_varint_block_unsigned(py: Python, ints: Vec<u64>) -> PyObject {
     let mut buffer = vec![0u8; ints.len() * 9];
     let mut blen = 0;
 
-    for int in ints.iter() {
-        let v: VarInt = (*int as i64).into();
-        blen += v.encode(&mut buffer[blen..(blen+9)]);
+    for int in ints {
+        blen += (int as i64).encode_varint(&mut buffer[blen..(blen + 9)]);
     }
 
     PyBytes::new(py, &buffer[..blen]).into()
@@ -56,24 +49,72 @@ fn ziggurat_varint(_py: Python, m: &PyModule) -> PyResult<()> {
     Ok(())
 }
 
-#[repr(transparent)]
-pub struct VarInt(i64);
+#[inline]
+pub fn decode(bytes: &[u8]) -> (i64, usize) {
+    let mut i = 0;
+    let mut output = (bytes[i] & 0b00111111) as i64;
+    let neg = (bytes[i] & 0b01000000) == 0b01000000;
+    let mut cont = (bytes[i] & 0b10000000) == 0b10000000;
 
-impl VarInt {
+    while cont && i < 7 {
+        i += 1;
+        output <<= 7;
+        output |= (bytes[i] & 0b01111111) as i64;
+        cont = (bytes[i] & 0b10000000) == 0b10000000;
+    }
 
+    if cont {
+        i += 1;
+        output <<= 8;
+        output |= (bytes[i] & 0xFF) as i64;
+    }
+
+    if neg {
+        (!output, i + 1)
+    } else {
+        (output, i + 1)
+    }
+}
+
+pub fn decode_block(bytes: &[u8]) -> Vec<i64> {
+    let mut offset = 0;
+    let mut output = Vec::new();
+    while offset < bytes.len() {
+        let (i, len) = decode(&bytes[offset..]);
+        output.push(i);
+        offset += len;
+    }
+    output
+}
+
+pub fn encode_block<I: EncodeVarint>(block: &[I]) -> Vec<u8> {
+    let mut output = Vec::with_capacity(block.len() * 9);
+    for i in block {
+        let mut bytes = [0u8; 9];
+        let len = i.encode_varint(&mut bytes[..]);
+        output.extend_from_slice(&bytes[..len]);
+    }
+    output
+}
+
+pub trait EncodeVarint {
+    fn encode_varint(&self, buffer: &mut [u8]) -> usize;
+}
+
+impl EncodeVarint for i64 {
     #[inline]
-    pub fn encode(&self, arr: &mut [u8]) -> usize {
-        assert!(arr.len() >= 9, "passed slice too small to hold VarInt");
-        
-        let VarInt(mut x) = *self;
+    fn encode_varint(&self, arr: &mut [u8]) -> usize {
+        assert!(arr.len() >= 9, "passed slice too small to hold varint");
+        let mut x = *self;
 
         let is_negative = x.is_negative();
-        if is_negative { // invert if negative
+        if is_negative {
+            // invert if negative
             x = !x;
         }
-        
+
         let mut mask = -1 << 6;
-    
+
         let mut n_bytes = 1; // find number of bytes needed
         while (x & mask) != 0 {
             match n_bytes {
@@ -82,25 +123,27 @@ impl VarInt {
             }
             n_bytes += 1;
         }
-    
+
         let mut k = n_bytes - 1;
-        
-        if n_bytes == 9 { // byte 9 with 8 bits
+
+        if n_bytes == 9 {
+            // byte 9 with 8 bits
             arr[k] = (x & 0xff) as u8;
             x >>= 8;
             k -= 1;
         }
-    
-        while k > 0 && k < 9 { // byte 2..8 with 7 bits
+
+        while k > 0 && k < 9 {
+            // byte 2..8 with 7 bits
             let mut byte = (x & 0x7f) as u8;
             x >>= 7;
-            if k < n_bytes-1 { 
+            if k < n_bytes - 1 {
                 byte |= 0x80;
             }
             arr[k] = byte;
             k -= 1;
         }
-    
+
         let mut byte = (x & 0x3f) as u8; // byte 1 with 6 bits
         if n_bytes > 1 {
             byte |= 0x80; // set continuation bit
@@ -109,34 +152,39 @@ impl VarInt {
             byte |= 0x40; // set sign bit
         }
         arr[0] = byte;
-    
+
         n_bytes
     }
-}
-
-impl From<i64> for VarInt {
-
-    #[inline]
-    fn from(value: i64) -> VarInt {
-        VarInt(value)
-    }
-
 }
 
 #[cfg(test)]
 mod tests {
 
-    use crate::VarInt;
-
-    #[test]
-    fn into() {
-        let a: i64 = 1337;
-        let v: VarInt = a.into();
-    }
+    use crate::EncodeVarint;
 
     #[test]
     fn boundaries() {
-        let ints: Vec<i64> = vec![0, -64, 63, -8192, 8191, -1048576, 1048575, -134217728, 134217727, -17179869184, 17179869183, -2199023255552, 2199023255551, -281474976710656, 281474976710655, -36028797018963968, 36028797018963967, -9223372036854775808, 9223372036854775807];
+        let ints: Vec<i64> = vec![
+            0,
+            -64,
+            63,
+            -8192,
+            8191,
+            -1048576,
+            1048575,
+            -134217728,
+            134217727,
+            -17179869184,
+            17179869183,
+            -2199023255552,
+            2199023255551,
+            -281474976710656,
+            281474976710655,
+            -36028797018963968,
+            36028797018963967,
+            -9223372036854775808,
+            9223372036854775807,
+        ];
 
         let expected_lens = vec![1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9];
 
@@ -166,9 +214,8 @@ mod tests {
         let mut encodings: Vec<Vec<u8>> = Vec::new();
 
         for i in ints {
-            let v: VarInt = i.into();
             let mut buffer = [0; 9];
-            let len = v.encode(&mut buffer);
+            let len = i.encode_varint(&mut buffer);
             lens.push(len);
             encodings.push(buffer[..len].into());
         }
