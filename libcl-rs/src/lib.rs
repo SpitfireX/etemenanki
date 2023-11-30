5c0a8506caaccd0a2fe7008218b0e32d8e8b2dc4
SpitfireX <timm.weber@me.com>
2023-11-30 17:03:31 +0100

    implemented rest of p attribute api

--- libcl-rs/src/lib.rs
+++ libcl-rs/src/lib.rs
@@ -1,14 +1,15 @@
 #![feature(test)]
 
+use core::fmt;
 use std::{
+    error::Error,
     ffi::{CStr, CString},
     path::Path,
 };
 
-use bindings::{
-    cl_corpus_list_attributes, cl_delete_corpus, cl_delete_string_list, cl_first_corpus_property,
-    cl_max_cpos, cl_max_id, cl_new_attribute, cl_string_list_get, cl_string_list_size,
-};
+use num_enum::TryFromPrimitive;
+
+use bindings::*;
 
 mod bindings {
     #![allow(non_upper_case_globals)]
@@ -220,18 +221,199 @@ impl Drop for Corpus {
     }
 }
 
+#[derive(Clone, Copy, Debug, TryFromPrimitive)]
+#[repr(i32)]
+pub enum DataAccessError {
+    OK = 0,                    // everything is fine; actual error values are all less than 0
+    ENULLATT = -1,             // NULL passed as attribute argument
+    EATTTYPE = -2,             // function was called on illegal attribute
+    EIDORNG = -3,              // id out of range
+    EPOSORNG = -4,             // position out of range
+    EIDXORNG = -5,             // index out of range
+    ENOSTRING = -6,            // no such string encoded
+    EPATTERN = -7,             // illegal pattern
+    ESTRUC = -8,               // no structure at position
+    EALIGN = -9,               // no alignment at position
+    EREMOTE = -10,             // error in remote access
+    ENODATA = -11,             // can't load/create necessary data
+    EARGS = -12,               // error in arguments for dynamic call or CL function
+    ENOMEM = -13,              // memory fault [unused]
+    EOTHER = -14,              // other error
+    ENYI = -15,                // not yet implemented
+    EBADREGEX = -16,           // bad regular expression
+    EFSETINV = -17,            // invalid feature set format
+    EBUFFER = -18,             // buffer overflow (hard-coded internal buffer sizes)
+    EINTERNAL = -19,           // internal data consistency error (really bad)
+    EACCESS = -20,             // insufficient access permissions
+    EPOSIX = -21,              // POSIX-level error: check errno or perror()
+    CPOSUNDEF = std::i32::MIN, // undefined corpus position (use this code to avoid ambiguity with negative cpos)
+}
+
+impl fmt::Display for DataAccessError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let str = unsafe {
+            let ptr = cl_error_string(*self as i32);
+            ptr_to_str(ptr)
+        };
+        match str {
+            Some(str) => f.write_str(str),
+            None => f.write_str("Unknown Error"),
+        }
+    }
+}
+
+impl Error for DataAccessError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
+
+macro_rules! cl_error_or {
+    ($ok:expr) => {{
+        let error = DataAccessError::try_from(cl_errno).expect("invalid cl_errno value");
+        match error {
+            DataAccessError::OK => Ok($ok),
+            _ => Err(error),
+        }
+    }};
+}
+
+type AccessResult<T> = Result<T, DataAccessError>;
+
+pub struct MallocSlice<'c, T> {
+    inner: &'c [T],
+}
+
+impl<'c, T> MallocSlice<'c, T> {
+    pub unsafe fn from_raw_parts(data: *const T, len: usize) -> Self {
+        Self {
+            inner: std::slice::from_raw_parts(data, len),
+        }
+    }
+}
+
+impl<'c, T> std::ops::Deref for MallocSlice<'c, T> {
+    type Target = [T];
+
+    fn deref(&self) -> &Self::Target {
+        self.inner
+    }
+}
+
+impl<'c, T> std::ops::Drop for MallocSlice<'c, T> {
+    fn drop(&mut self) {
+        unsafe {
+            let ptr = self.inner.as_ptr() as *mut libc::c_void;
+            libc::free(ptr);
+        }
+    }
+}
+
 pub struct PositionalAttribute<'c> {
     ptr: *mut bindings::Attribute,
     _parent: &'c Corpus,
 }
 
 impl<'c> PositionalAttribute<'c> {
-    pub fn max_cpos(&self) -> usize {
-        unsafe { cl_max_cpos(self.ptr) as usize }
+    pub fn id2str(&self, id: i32) -> AccessResult<&'c CStr> {
+        unsafe {
+            let ptr = cl_id2str(self.ptr, id);
+            cl_error_or!(CStr::from_ptr(ptr))
+        }
     }
 
-    pub fn max_id(&self) -> usize {
-        unsafe { cl_max_id(self.ptr) as usize }
+    pub fn str2id(&self, str: &CStr) -> AccessResult<i32> {
+        unsafe { cl_error_or!(cl_str2id(self.ptr, str.as_ptr() as *mut i8)) }
+    }
+
+    pub fn id2strlen(&self, id: i32) -> AccessResult<i32> {
+        unsafe { cl_error_or!(cl_id2strlen(self.ptr, id)) }
+    }
+
+    pub fn sort2id(&self, sort_index_position: i32) -> AccessResult<i32> {
+        unsafe { cl_error_or!(cl_sort2id(self.ptr, sort_index_position)) }
+    }
+
+    pub fn id2sort(&self, id: i32) -> AccessResult<i32> {
+        unsafe { cl_error_or!(cl_id2sort(self.ptr, id)) }
+    }
+
+    pub fn max_cpos(&self) -> AccessResult<i32> {
+        unsafe { cl_error_or!(cl_max_cpos(self.ptr)) }
+    }
+
+    pub fn max_id(&self) -> AccessResult<i32> {
+        unsafe { cl_error_or!(cl_max_id(self.ptr)) }
+    }
+
+    pub fn id2freq(&self, id: i32) -> AccessResult<i32> {
+        unsafe { cl_error_or!(cl_id2freq(self.ptr, id)) }
+    }
+
+    pub fn id2cpos(&self, id: i32) -> AccessResult<MallocSlice<i32>> {
+        unsafe {
+            let mut freq = 0;
+            let ptr = cl_id2cpos_oldstyle(self.ptr, id, &mut freq, core::ptr::null_mut(), 0);
+            cl_error_or!(MallocSlice::from_raw_parts(ptr, freq as usize))
+        }
+    }
+
+    pub fn cpos2id(&self, position: i32) -> AccessResult<i32> {
+        unsafe { cl_error_or!(cl_cpos2id(self.ptr, position)) }
+    }
+
+    pub fn cpos2str(&self, position: i32) -> AccessResult<&'c CStr> {
+        unsafe {
+            let ptr = cl_cpos2str(self.ptr, position);
+            cl_error_or!(CStr::from_ptr(ptr))
+        }
+    }
+
+    pub fn id2all(&self, id: i32) -> AccessResult<(&'c CStr, i32, i32)> {
+        unsafe {
+            let mut slen = 0;
+            let mut freq = 0;
+            let ptr = cl_id2all(self.ptr, id, &mut freq, &mut slen);
+            cl_error_or!((CStr::from_ptr(ptr), slen, freq))
+        }
+    }
+
+    pub fn regex2id(&self, pattern: &CStr, flags: i32) -> AccessResult<Option<MallocSlice<i32>>> {
+        unsafe {
+            let mut len = 0;
+            let ptr = cl_regex2id(self.ptr, pattern.as_ptr() as *mut i8, flags, &mut len);
+            if ptr.is_null() {
+                cl_error_or!(None)
+            } else {
+                cl_error_or!(Some(MallocSlice::from_raw_parts(ptr, len as usize)))
+            }
+        }
+    }
+
+    pub fn idlist2freq(&self, idlist: &[i32]) -> AccessResult<i32> {
+        unsafe {
+            cl_error_or!(cl_idlist2freq(
+                self.ptr,
+                idlist.as_ptr() as *mut _,
+                idlist.len() as i32
+            ))
+        }
+    }
+
+    pub fn idlist2cpos(&self, idlist: &[i32], sort: bool) -> AccessResult<MallocSlice<i32>> {
+        unsafe {
+            let mut len = 0;
+            let ptr = cl_idlist2cpos_oldstyle(
+                self.ptr,
+                idlist.as_ptr() as *mut _,
+                idlist.len() as i32,
+                sort as i32,
+                &mut len,
+                core::ptr::null_mut(),
+                0,
+            );
+            cl_error_or!(MallocSlice::from_raw_parts(ptr, len as usize))
+        }
     }
 }
 
@@ -286,12 +468,12 @@ mod tests {
         let c = Corpus::new("testdata/registry", "simpledickens").expect("Could not open corpus");
 
         let word = c.get_p_attribute("word").unwrap();
-        assert!((word.max_cpos(), word.max_id()) == (3407085, 57568));
+        assert!((word.max_cpos().unwrap(), word.max_id().unwrap()) == (3407085, 57568));
 
         let pos = c.get_p_attribute("pos").unwrap();
-        assert!((pos.max_cpos(), pos.max_id()) == (3407085, 43));
+        assert!((pos.max_cpos().unwrap(), pos.max_id().unwrap()) == (3407085, 43));
 
         let lemma = c.get_p_attribute("lemma").unwrap();
-        assert!((lemma.max_cpos(), lemma.max_id()) == (3407085, 41222));
+        assert!((lemma.max_cpos().unwrap(), lemma.max_id().unwrap()) == (3407085, 41222));
     }
 }
