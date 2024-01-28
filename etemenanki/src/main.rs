8fdd98971bcf7605b3cef300e596a8f7ab2e89e4
SpitfireX <timm.weber@me.com>
2024-01-28 19:51:09 +0100

    new CachedVector rough implementation

--- etemenanki/src/main.rs
+++ etemenanki/src/main.rs
@@ -12,7 +12,7 @@ use etemenanki::container::Container;
 use etemenanki::{container, Datastore};
 use lru::LruCache;
 use memmap2::Mmap;
-use streaming_iterator::StreamingIteratorMut;
+use streaming_iterator::StreamingIterator;
 
 fn main() -> Result<()> {
     // let args: Vec<String> = env::args().collect();
@@ -135,7 +135,7 @@ fn main() -> Result<()> {
     let filename = "../scripts/recipes4000/s/s.zigl";
     let file = File::open(filename).unwrap();
     let mmap = unsafe { Mmap::map(&file) }.unwrap();
-    let mut container = Container::from_mmap(mmap, "word".to_owned()).unwrap();
+    let container = Container::from_mmap(mmap, "word".to_owned()).unwrap();
 
     let vector = container
         .components
@@ -145,10 +145,13 @@ fn main() -> Result<()> {
         .unwrap();
 
     let mut cached = CachedVector::new(*vector);
+    let mut iter = cached.column_iter_range(0, 10, 15).unwrap();
 
-    for i in 0..20 {
-        println!("{:?}", cached.get_row_unchecked(i));
+    while let Some(row) = iter.next() {
+        println!("{:?}", row);
     }
+    println!("loop done");
+    println!("{:?}", iter.next());
 
     Ok(())
 }
@@ -159,6 +162,44 @@ struct CachedVector<'map> {
 }
 
 impl<'map> CachedVector<'map> {
+    /// Decodes a compressed block and returns it as a contiguous Vec of dimension n*d in row major order.
+    fn decode_compressed_block(d: usize, raw_data: &[u8]) -> Vec<i64> {
+        let mut block = vec![0i64; d * 16];
+        let mut offset = 0;
+
+        for i in 0..d {
+            for j in 0..16 {
+                let (int, len) = ziggurat_varint::decode(&raw_data[offset..]);
+                block[(j * d) + i] = int; // wonky because conversion from col-major to row-major
+                offset += len;
+            }
+        }
+
+        block
+    }
+
+    /// Decodes a delta compressed block and returns it as a contiguous Vec of dimension n*d in row-major order.
+    fn decode_delta_block(d: usize, raw_data: &[u8]) -> Vec<i64> {
+        let mut delta_block = vec![0i64; d * 16];
+        let mut offset = 0;
+
+        for i in 0..d {
+            for j in 0..16 {
+                let (int, len) = ziggurat_varint::decode(&raw_data[offset..]);
+                let current = (j * d) + i;
+                if j == 0 {
+                    delta_block[current] = int; // initial seed values
+                } else {
+                    let last = ((j - 1) * d) + i;
+                    delta_block[current] = delta_block[last] + int;
+                }
+                offset += len;
+            }
+        }
+
+        delta_block
+    }
+
     pub fn new(vector: Vector<'map>) -> Self {
         Self {
             inner: vector,
@@ -174,67 +215,39 @@ impl<'map> CachedVector<'map> {
         self.inner.width()
     }
 
-    fn decode_compressed_block(d: usize, raw_data: &[u8]) -> Vec<i64> {
-        let mut block = vec![0i64; d*16];
-
-        let mut offset = 0;
-        for i in 0..d {
-            for j in 0..16 {
-                let (int, len) = ziggurat_varint::decode(&raw_data[offset..]);
-                block[(j*d) + i] = int; // wonky because conversion from col-major to row-major
-                offset += len;
-            }
-        }
-
-        block
-    }
-
-    fn decode_delta_block(d: usize, raw_data: &[u8]) -> Vec<i64> {
-        let mut delta_block = Self::decode_compressed_block(d, raw_data);
-
-        for i in 0..d {
-            for j in 1..16 {
-                let current = (j*d) + i;
-                let last = ((j-1)*d) + i;
-                delta_block[current] = delta_block[last] + delta_block[current];
-            }
-        }
-
-        delta_block
-    }
-
-    fn get_block(&mut self, block_index: usize) -> &[i64] {
-        if !self.cache.contains(&block_index) {
-            println!("cache miss: {}", block_index);
-            let block = match self.inner {
-                Vector::Uncompressed { .. } => panic!("n/a for uncompressed vector"),
-                
-                Vector::Compressed { length: _, width: _, sync, data } => {
-                    let block_start = sync[block_index] as usize;
-                    Self::decode_compressed_block(self.width(), &data[block_start..])
-                }
-
-                Vector::Delta { length: _, width: _, sync, data } => {
-                    let block_start = sync[block_index] as usize;
-                    Self::decode_delta_block(self.width(), &data[block_start..])
-                }
-            };
-
-            self.cache.put(block_index, block);
-        }
-
-        let cached_block = self
-            .cache
-            .get(&block_index)
-            .expect("at this point a block must be already cached");
-        &cached_block[..]
-    }
-
     pub fn get_row(&mut self, index: usize) -> Option<&[i64]> {
-        if index < self.len() * self.width() {
+        if index < self.len() {
             Some(self.get_row_unchecked(index))
         } else {
             None
+        }
+    }
+
+    fn row_index_to_block_offsets(&self, index: usize) -> (usize, usize, usize) {
+        let bi = index / 16;
+        let start = (index % 16) * self.width();
+        let end = start + self.width();
+        (bi, start, end)
+    }
+
+    // Returns Some(row) if the row is immediately available (either uncompressed or already decoded in the cache)
+    // else None. This method never decodes any new blocks and doesn't modify the cache's LRU list.
+    fn peek_row (&self, index: usize) -> Option<&[i64]> {
+        match self.inner {
+            Vector::Uncompressed { .. } => {
+                let row = self.inner.get_row_unchecked(index);
+                if let VecSlice::Borrowed(s) = row {
+                    Some(s)
+                } else {
+                    panic!("in case of uncompressed vec the row must be a slice into the mmap");
+                }
+            }
+
+            Vector::Compressed { .. } | Vector::Delta { .. } => {
+                let (bi, start, end) = self.row_index_to_block_offsets(index);
+                self.cache.peek(&bi)
+                    .map(|b| &b[start..end])
+            }
         }
     }
 
@@ -249,31 +262,155 @@ impl<'map> CachedVector<'map> {
                 }
             }
             Vector::Delta { .. } | Vector::Compressed { .. } => {
-                let start = (index % 16) * self.width();
-                let end = start + self.width();
-                let bi = index / 16;
-                let block = self.get_block(bi);
+                let (bi, start, end) = self.row_index_to_block_offsets(index);
+
+                // decode and cache block if needed
+                if !self.cache.contains(&bi) {
+                    println!("cache miss, decoding block {}", bi);
+                    let block = match self.inner {
+                        Vector::Uncompressed { .. } => panic!("n/a for uncompressed vector"),
+                        
+                        Vector::Compressed { length: _, width: _, sync, data } => {
+                            let block_start = sync[bi] as usize;
+                            Self::decode_compressed_block(self.width(), &data[block_start..])
+                        }
+        
+                        Vector::Delta { length: _, width: _, sync, data } => {
+                            let block_start = sync[bi] as usize;
+                            Self::decode_delta_block(self.width(), &data[block_start..])
+                        }
+                    };
+        
+                    self.cache.put(bi, block);
+                }
+
+                // return reference into cache
+                let block = self.cache
+                    .get(&bi)
+                    .expect("at this point the block must be cached");
                 &block[start..end]
             }
         }
     }
 
-    pub fn iter(&mut self) -> CachedIter {
-        CachedIter {
-            cvec: self,
-            position: 0,
-            end: 10,
+    pub fn column_iter(&mut self, column: usize) -> Option<ColumnIter<'_, 'map>> {
+        let len = self.len();
+        if column < self.width() {
+            Some(ColumnIter::new(column, 0, len, self))
+        } else {
+            None
+        }
+    }
+
+    pub fn column_iter_from(&mut self, column: usize, start: usize) -> Option<ColumnIter<'_, 'map>> {
+        let len = self.len();
+        if column < self.width() {
+            Some(ColumnIter::new(column, start, len, self))
+        } else {
+            None
+        }
+    }
+
+    pub fn column_iter_range(&mut self, column: usize, start: usize, end: usize) -> Option<ColumnIter<'_, 'map>> {
+        let len = self.len();
+        if column < self.width() && end < len {
+            Some(ColumnIter::new(column, start, end, self))
+        } else {
+            None
+        }
+    }
+
+    pub fn iter(&mut self) -> RowIter<'_, 'map> {
+        let len = self.len();
+        RowIter::new(0, len, self)
+    }
+
+    pub fn iter_from(&mut self, start: usize) -> RowIter<'_, 'map> {
+        let len = self.len();
+        RowIter::new(start, len, self)
+    }
+
+    pub fn iter_range(&mut self, start: usize, end: usize) -> Option<RowIter<'_, 'map>> {
+        let len = self.len();
+        if end <= len {
+            Some(RowIter::new(start, end, self))
+        } else {
+            None
         }
     }
 }
 
-struct CachedIter<'cv, 'map> {
+struct ColumnIter<'cv, 'map> {
+    cvec: &'cv mut CachedVector<'map>,
+    col: usize,
+    position: usize,
+    end: usize,
+}
+
+impl <'cv, 'map> ColumnIter<'cv, 'map> {
+    pub fn new(column: usize, start: usize, end: usize, cvec: &'cv mut CachedVector<'map>) -> Self {
+        Self {
+            cvec,
+            col: column,
+            position: start,
+            end,
+        }
+    }
+}
+
+impl <'cv, 'map> StreamingIterator for ColumnIter<'cv, 'map> {
+    type Item = i64;
+
+    fn advance(&mut self) {
+        if self.position <= self. end {
+            self.cvec.get_row(self.position);
+            self.position += 1;
+        }
+    }
+
+    fn get(&self) -> Option<&Self::Item> {
+        if self.position <= self.end {
+            let (bi, start, _) = self.cvec.row_index_to_block_offsets(self.position - 1);
+            self.cvec
+                .cache.peek(&bi)
+                .map(|b| &b[start + self.col])
+        } else {
+            None
+        }
+    }
+}
+
+struct RowIter<'cv, 'map> {
     cvec: &'cv mut CachedVector<'map>,
     position: usize,
     end: usize,
 }
-impl<'cv, 'map> StreamingIteratorMut for CachedIter<'cv, 'map> {
-    fn get_mut(&mut self) -> Option<&mut Self::Item> {
-        todo!()
+
+impl <'cv, 'map> RowIter<'cv, 'map> {
+    pub fn new(start: usize, end: usize, cvec: &'cv mut CachedVector<'map>) -> Self {
+        Self {
+            cvec,
+            position: start,
+            end
+        }
+    }
+}
+
+impl <'cv, 'map> StreamingIterator for RowIter<'cv, 'map> {
+    type Item = [i64];
+
+    fn advance(&mut self) {
+        if self.position <= self.end {
+            self.cvec.get_row(self.position);
+            self.position += 1;
+        }
+    }
+
+    fn get(&self) -> Option<&Self::Item> {
+        if self.position <= self.end {
+            self.cvec.peek_row(self.position-1) // -1 because get always gets called after advace
+        } else {
+            None
+        }
     }
 }
